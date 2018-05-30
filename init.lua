berlam/awesome-switcher

commit 9c533bea9cd407a46c0d5dad19b484d29c2706fd

Add compatibility for Lua <5.3

init.lua
@@ -17,16 +17,16 @@ local tostring = tostring
 local tonumber = tonumber
 local debug = debug
 local pairs = pairs
-local unpack = unpack
-
-module("awesome-switcher-preview")
+local unpack = unpack or table.unpack
 
 local surface = cairo.ImageSurface(cairo.Format.RGB24,20,20)
 local cr = cairo.Context(surface)
 
+local _M = {}
+
 -- settings
 
-local settings = {
+_M.settings = {
 	preview_box = true,
 	preview_box_bg = "#ddddddaa",
 	preview_box_border = "#22222200",
@@ -45,30 +45,30 @@ local settings = {
 }
 
 -- Create a wibox to contain all the client-widgets
-local preview_wbox = wibox({ width = screen[mouse.screen].geometry.width })
-preview_wbox.border_width = 3
-preview_wbox.ontop = true
-preview_wbox.visible = false
+_M.preview_wbox = wibox({ width = screen[mouse.screen].geometry.width })
+_M.preview_wbox.border_width = 3
+_M.preview_wbox.ontop = true
+_M.preview_wbox.visible = false
 
-local preview_live_timer = timer({ timeout = 1/settings.preview_box_fps })
-local preview_widgets = {}
+_M.preview_live_timer = timer({ timeout = 1/_M.settings.preview_box_fps })
+_M.preview_widgets = {}
 
-local altTabTable = {}
-local altTabIndex = 1
+_M.altTabTable = {}
+_M.altTabIndex = 1
 
-local source = string.sub(debug.getinfo(1,'S').source, 2)
-local path = string.sub(source, 1, string.find(source, "/[^/]*$"))
-local noicon = path .. "noicon.png"
+_M.source = string.sub(debug.getinfo(1,'S').source, 2)
+_M.path = string.sub(_M.source, 1, string.find(_M.source, "/[^/]*$"))
+_M.noicon = _M.path .. "noicon.png"
 
 -- simple function for counting the size of a table
-local function tableLength(T)
+function _M.tableLength(T)
 	local count = 0
 	for _ in pairs(T) do count = count + 1 end
 	return count
 end
 
 -- this function returns the list of clients to be shown.
-local function getClients()
+function _M.getClients()
 	local clients = {}
 
 	-- Get focus history for current tag
@@ -126,30 +126,30 @@ local function getClients()
 end
 
 -- here we populate altTabTable using the list of clients taken from
--- getClients(). In case we have altTabTable with some value, the list of the
+-- _M.getClients(). In case we have altTabTable with some value, the list of the
 -- old known clients is restored.
-local function populateAltTabTable()
-	local clients = getClients()
+function _M.populateAltTabTable()
+	local clients = _M.getClients()
 
-	if tableLength(altTabTable) then
+	if _M.tableLength(_M.altTabTable) then
 		for ci = 1, #clients do
-			for ti = 1, #altTabTable do
-				if altTabTable[ti].client == clients[ci] then
-					altTabTable[ti].client.opacity = altTabTable[ti].opacity
-					altTabTable[ti].client.minimized = altTabTable[ti].minimized
+			for ti = 1, #_M.altTabTable do
+				if _M.altTabTable[ti].client == clients[ci] then
+					_M.altTabTable[ti].client.opacity = _M.altTabTable[ti].opacity
+					_M.altTabTable[ti].client.minimized = _M.altTabTable[ti].minimized
 					break
 				end
 			end
 		end
 	end
 
-	altTabTable = {}
+	_M.altTabTable = {}
 
 	for i = 1, #clients do
-		table.insert(altTabTable, {
-	       client = clients[i],
-	       minimized = clients[i].minimized,
-	       opacity = clients[i].opacity
+		table.insert(_M.altTabTable, {
+		client = clients[i],
+		minimized = clients[i].minimized,
+		opacity = clients[i].opacity
        })
 	end
 end
@@ -157,12 +157,12 @@ end
 -- If the length of list of clients is not equal to the length of altTabTable,
 -- we need to repopulate the array and update the UI. This function does this
 -- check.
-local function clientsHaveChanged()
-	local clients = getClients()
-	return tableLength(clients) ~= tableLength(altTabTable)
+function _M.clientsHaveChanged()
+	local clients = _M.getClients()
+	return _M.tableLength(clients) ~= _M.tableLength(_M.altTabTable)
 end
 
-local function createPreviewText(client)
+function _M.createPreviewText(client)
 	if client.class then
 		return " - " .. client.class
 	else
@@ -171,10 +171,10 @@ local function createPreviewText(client)
 end
 
 -- Preview is created here.
-local function clientOpacity(altTabTable, altTabIndex)
-	if not settings.client_opacity then return end
+function _M.clientOpacity(altTabTable, altTabIndex)
+	if not _M.settings.client_opacity then return end
 
-	local opacity = settings.client_opacity_value
+	local opacity = _M.settings.client_opacity_value
 	if opacity > 1 then opacity = 1 end
 	for i,data in pairs(altTabTable) do
 		data.client.opacity = opacity
@@ -182,14 +182,14 @@ local function clientOpacity(altTabTable, altTabIndex)
 
 	if client.focus == altTabTable[altTabIndex].client then
 		-- Let's normalize the value up to 1.
-		local opacityFocusSelected = settings.client_opacity_value_selected + settings.client_opacity_value_in_focus
+		local opacityFocusSelected = _M.settings.client_opacity_value_selected + _M.settings.client_opacity_value_in_focus
 		if opacityFocusSelected > 1 then opacityFocusSelected = 1 end
 		client.focus.opacity = opacityFocusSelected
 	else
 		-- Let's normalize the value up to 1.
-		local opacityFocus = settings.client_opacity_value_in_focus
+		local opacityFocus = _M.settings.client_opacity_value_in_focus
 		if opacityFocus > 1 then opacityFocus = 1 end
-		local opacitySelected = settings.client_opacity_value_selected
+		local opacitySelected = _M.settings.client_opacity_value_selected
 		if opacitySelected > 1 then opacitySelected = 1 end
 
 		client.focus.opacity = opacityFocus
@@ -197,83 +197,83 @@ local function clientOpacity(altTabTable, altTabIndex)
 	end
 end
 
--- This is called any settings.preview_box_fps milliseconds. In case the list
+-- This is called any _M.settings.preview_box_fps milliseconds. In case the list
 -- of clients is changed, we need to redraw the whole preview box. Otherwise, a
 -- simple widget::updated signal is enough
-local function updatePreview()
-	if clientsHaveChanged() then
-		populateAltTabTable()
-		preview()
+function _M.updatePreview()
+	if _M.clientsHaveChanged() then
+		_M.populateAltTabTable()
+		_M.preview()
 	end
 
-	for i = 1, #preview_widgets do
-		preview_widgets[i]:emit_signal("widget::updated")
+	for i = 1, #_M.preview_widgets do
+		_M.preview_widgets[i]:emit_signal("widget::updated")
 	end
 end
 
-local function cycle(dir)
+function _M.cycle(dir)
 	-- Switch to next client
-	altTabIndex = altTabIndex + dir
-	if altTabIndex > #altTabTable then
-		altTabIndex = 1 -- wrap around
-	elseif altTabIndex < 1 then
-		altTabIndex = #altTabTable -- wrap around
+	_M.altTabIndex = _M.altTabIndex + dir
+	if _M.altTabIndex > #_M.altTabTable then
+		_M.altTabIndex = 1 -- wrap around
+	elseif _M.altTabIndex < 1 then
+		_M.altTabIndex = #_M.altTabTable -- wrap around
 	end
 
-	updatePreview()
+	_M.updatePreview()
 
-	altTabTable[altTabIndex].client.minimized = false
+	_M.altTabTable[_M.altTabIndex].client.minimized = false
 
-	if not settings.preview_box and not settings.client_opacity then
-		client.focus = altTabTable[altTabIndex].client
+	if not _M.settings.preview_box and not _M.settings.client_opacity then
+		client.focus = _M.altTabTable[_M.altTabIndex].client
 	end
 
-	if settings.client_opacity and preview_wbox.visible then
-		clientOpacity()
+	if _M.settings.client_opacity and _M.preview_wbox.visible then
+		_M.clientOpacity()
 	end
 
-	if settings.cycle_raise_client == true then
-		altTabTable[altTabIndex].client:raise()
+	if _M.settings.cycle_raise_client == true then
+		_M.altTabTable[_M.altTabIndex].client:raise()
 	end
 end
 
-local function preview()
-	if not settings.preview_box then return end
+function _M.preview()
+	if not _M.settings.preview_box then return end
 
 	-- Apply settings
-	preview_wbox:set_bg(settings.preview_box_bg)
-	preview_wbox.border_color = settings.preview_box_border
+	_M.preview_wbox:set_bg(_M.settings.preview_box_bg)
+	_M.preview_wbox.border_color = _M.settings.preview_box_border
 
 	-- Make the wibox the right size, based on the number of clients
-	local n = math.max(7, #altTabTable)
-	local W = screen[mouse.screen].geometry.width -- + 2 * preview_wbox.border_width
+	local n = math.max(7, #_M.altTabTable)
+	local W = screen[mouse.screen].geometry.width -- + 2 * _M.preview_wbox.border_width
 	local w = W / n -- widget width
 	local h = w * 0.75  -- widget height
 	local textboxHeight = w * 0.125
 
-	local x = screen[mouse.screen].geometry.x - preview_wbox.border_width
+	local x = screen[mouse.screen].geometry.x - _M.preview_wbox.border_width
 	local y = screen[mouse.screen].geometry.y + (screen[mouse.screen].geometry.height - h - textboxHeight) / 2
-	preview_wbox:geometry({x = x, y = y, width = W, height = h + textboxHeight})
+	_M.preview_wbox:geometry({x = x, y = y, width = W, height = h + textboxHeight})
 
 	-- create a list that holds the clients to preview, from left to right
 	local leftRightTab = {}
 	local leftRightTabToAltTabIndex = {} -- save mapping from leftRightTab to altTabTable as well
 	local nLeft
 	local nRight
-	if #altTabTable == 2 then
+	if #_M.altTabTable == 2 then
 		nLeft = 0
 		nRight = 2
 	else
-		nLeft = math.floor(#altTabTable / 2)
-		nRight = math.ceil(#altTabTable / 2)
+		nLeft = math.floor(#_M.altTabTable / 2)
+		nRight = math.ceil(#_M.altTabTable / 2)
 	end
 
 	for i = 1, nLeft do
-		table.insert(leftRightTab, altTabTable[#altTabTable - nLeft + i].client)
-		table.insert(leftRightTabToAltTabIndex, #altTabTable - nLeft + i)
+		table.insert(leftRightTab, _M.altTabTable[#_M.altTabTable - nLeft + i].client)
+		table.insert(leftRightTabToAltTabIndex, #_M.altTabTable - nLeft + i)
 	end
 	for i = 1, nRight do
-		table.insert(leftRightTab, altTabTable[i].client)
+		table.insert(leftRightTab, _M.altTabTable[i].client)
 		table.insert(leftRightTabToAltTabIndex, i)
 	end
 
@@ -284,7 +284,7 @@ local function preview()
 	local bigFont = textboxHeight / 2
 	cr:set_font_size(fontSize)
 	for i = 1, #leftRightTab do
-		text = createPreviewText(leftRightTab[i])
+		text = _M.createPreviewText(leftRightTab[i])
 		textWidth = cr:text_extents(text).width
 		textHeight = cr:text_extents(text).height
 		if textWidth > maxTextWidth or textHeight > maxTextHeight then
@@ -305,25 +305,24 @@ local function preview()
 
 		bigFont = bigFont - 1
 	end
-	local smallFont = bigFont * settings.preview_box_title_font_size_factor
+	local smallFont = bigFont * _M.settings.preview_box_title_font_size_factor
 
-	preview_widgets = {}
+	_M.preview_widgets = {}
 
 	-- create all the widgets
 	for i = 1, #leftRightTab do
-		preview_widgets[i] = wibox.widget.base.make_widget()
-		preview_widgets[i].fit = function(preview_widget, width, height)
+		_M.preview_widgets[i] = wibox.widget.base.make_widget()
+		_M.preview_widgets[i].fit = function(preview_widget, width, height)
 			return w, h
 		end
 		local c = leftRightTab[i]
-		preview_widgets[i].draw = function(preview_widget, preview_wbox, cr, width, height)
+		_M.preview_widgets[i].draw = function(preview_widget, preview_wbox, cr, width, height)
 			if width ~= 0 and height ~= 0 then
-
 
 				local a = 0.8
 				local overlay = 0.6
 				local fontSize = smallFont
-				if c == altTabTable[altTabIndex].client then
+				if c == _M.altTabTable[_M.altTabIndex].client then
 					a = 0.9
 					overlay = 0
 					fontSize = bigFont
@@ -334,7 +333,7 @@ local function preview()
 				-- Icons
 				local icon
 				if c.icon == nil then
-					icon = gears.surface(gears.surface.load(noicon))
+					icon = gears.surface(gears.surface.load(_M.noicon))
 				else
 					icon = gears.surface(c.icon)
 				end
@@ -343,11 +342,11 @@ local function preview()
 				local iconboxHeight = iconboxWidth
 
 				-- Titles
-				cr:select_font_face(unpack(settings.preview_box_title_font))
+				cr:select_font_face(unpack(_M.settings.preview_box_title_font))
 				cr:set_font_face(cr:get_font_face())
 				cr:set_font_size(fontSize)
 
-				text = createPreviewText(c)
+				text = _M.createPreviewText(c)
 				textWidth = cr:text_extents(text).width
 				textHeight = cr:text_extents(text).height
 
@@ -371,7 +370,7 @@ local function preview()
 				tx = tx + iconboxWidth
 				ty = h + (textboxHeight + textHeight) / 2
 
-				cr:set_source_rgba(unpack(settings.preview_box_title_color))
+				cr:set_source_rgba(unpack(_M.settings.preview_box_title_color))
 				cr:move_to(tx, ty)
 				cr:show_text(text)
 				cr:stroke()
@@ -406,15 +405,15 @@ local function preview()
 		end
 
 		-- Add mouse handler
-		preview_widgets[i]:connect_signal("mouse::enter", function()
-			cycle(leftRightTabToAltTabIndex[i] - altTabIndex)
+		_M.preview_widgets[i]:connect_signal("mouse::enter", function()
+			_M.cycle(leftRightTabToAltTabIndex[i] - _M.altTabIndex)
 		end)
 	end
 
 	-- Spacers left and right
 	local spacer = wibox.widget.base.make_widget()
 	spacer.fit = function(leftSpacer, width, height)
-		return (W - w * #altTabTable) / 2, preview_wbox.height
+		return (W - w * #_M.altTabTable) / 2, _M.preview_wbox.height
 	end
 	spacer.draw = function(preview_widget, preview_wbox, cr, width, height) end
 
@@ -423,48 +422,48 @@ local function preview()
 
 	preview_layout:add(spacer)
 	for i = 1, #leftRightTab do
-		preview_layout:add(preview_widgets[i])
+		preview_layout:add(_M.preview_widgets[i])
 	end
 	preview_layout:add(spacer)
 
-	preview_wbox:set_widget(preview_layout)
+	_M.preview_wbox:set_widget(preview_layout)
 end
 
 
 -- This starts the timer for updating and it shows the preview UI.
-local function showPreview()
-	preview_live_timer.timeout = 1 / settings.preview_box_fps
-	preview_live_timer:connect_signal("timeout", updatePreview)
-	preview_live_timer:start()
+function _M.showPreview()
+	_M.preview_live_timer.timeout = 1 / _M.settings.preview_box_fps
+	_M.preview_live_timer:connect_signal("timeout", _M.updatePreview)
+	_M.preview_live_timer:start()
 
-	preview()
-	preview_wbox.visible = true
+	_M.preview()
+	_M.preview_wbox.visible = true
 
-	clientOpacity()
+	_M.clientOpacity()
 end
 
-local function switch(dir, mod_key1, release_key, mod_key2, key_switch)
-	populateAltTabTable()
+function _M.switch(dir, mod_key1, release_key, mod_key2, key_switch)
+	_M.populateAltTabTable()
 
-	if #altTabTable == 0 then
+	if #_M.altTabTable == 0 then
 		return
-	elseif #altTabTable == 1 then
-		altTabTable[1].client.minimized = false
-		altTabTable[1].client:raise()
+	elseif #_M.altTabTable == 1 then
+		_M.altTabTable[1].client.minimized = false
+		_M.altTabTable[1].client:raise()
 		return
 	end
 
 	-- reset index
-	altTabIndex = 1
+	_M.altTabIndex = 1
 
 	-- preview delay timer
-	local previewDelay = settings.preview_box_delay / 1000
-	local previewDelayTimer = timer({timeout = previewDelay})
-	previewDelayTimer:connect_signal("timeout", function()
-		previewDelayTimer:stop()
-		showPreview()
+	local previewDelay = _M.settings.preview_box_delay / 1000
+	_M.previewDelayTimer = timer({timeout = previewDelay})
+	_M.previewDelayTimer:connect_signal("timeout", function()
+		_M.previewDelayTimer:stop()
+		_M.showPreview()
 	end)
-	previewDelayTimer:start()
+	_M.previewDelayTimer:start()
 
 	-- Now that we have collected all windows, we should run a keygrabber
 	-- as long as the user is alt-tabbing:
@@ -473,40 +472,40 @@ local function switch(dir, mod_key1, release_key, mod_key2, key_switch)
 			-- Stop alt-tabbing when the alt-key is released
 			if gears.table.hasitem(mod, mod_key1) then
 				if (#mod == 1 and key == release_key) or key == "Escape" and event == "release" then
-					if preview_wbox.visible == true then
-						preview_wbox.visible = false
-						preview_live_timer:stop()
+					if _M.preview_wbox.visible == true then
+						_M.preview_wbox.visible = false
+						_M.preview_live_timer:stop()
 					else
-						previewDelayTimer:stop()
+						_M.previewDelayTimer:stop()
 					end
 
 					if key == "Escape" then
-						for i = 1, #altTabTable do
-							altTabTable[i].client.opacity = altTabTable[i].opacity
-							altTabTable[i].client.minimized = altTabTable[i].minimized
+						for i = 1, #_M.altTabTable do
+							_M.altTabTable[i].client.opacity = _M.altTabTable[i].opacity
+							_M.altTabTable[i].client.minimized = _M.altTabTable[i].minimized
 						end
 					else
 						-- Raise clients in order to restore history
 						local c
-						for i = 1, altTabIndex - 1 do
-							c = altTabTable[altTabIndex - i].client
-							if not altTabTable[i].minimized then
+						for i = 1, _M.altTabIndex - 1 do
+							c = _M.altTabTable[_M.altTabIndex - i].client
+							if not _M.altTabTable[i].minimized then
 								c:raise()
 								client.focus = c
 							end
 						end
 
 						-- raise chosen client on top of all
-						c = altTabTable[altTabIndex].client
+						c = _M.altTabTable[_M.altTabIndex].client
 						c:raise()
 						client.focus = c
 
 						-- restore minimized clients
-						for i = 1, #altTabTable do
-							if i ~= altTabIndex and altTabTable[i].minimized then
-								altTabTable[i].client.minimized = true
+						for i = 1, #_M.altTabTable do
+							if i ~= _M.altTabIndex and _M.altTabTable[i].minimized then
+								_M.altTabTable[i].client.minimized = true
 							end
-							altTabTable[i].client.opacity = altTabTable[i].opacity
+							_M.altTabTable[i].client.opacity = _M.altTabTable[i].opacity
 						end
 					end
 
@@ -515,10 +514,10 @@ local function switch(dir, mod_key1, release_key, mod_key2, key_switch)
 				elseif key == key_switch and event == "press" then
 					if gears.table.hasitem(mod, mod_key2) then
 						-- Move to previous client on Shift-Tab
-						cycle(-1)
+						_M.cycle(-1)
 					else
 						-- Move to next client on each Tab-press
-						cycle( 1)
+						_M.cycle( 1)
 					end
 				end
 			end
@@ -526,8 +525,8 @@ local function switch(dir, mod_key1, release_key, mod_key2, key_switch)
 	)
 
 	-- switch to next client
-	cycle(dir)
+	_M.cycle(dir)
 
 end -- function altTab
 
-return {switch = switch, settings = settings}
+return {switch = _M.switch, settings = _M.settings}

commit 87c13612111a0dbcbe34f00cca892feea0908aa3

Fix crash when client_opacity setting is enabled
Looks like it has been broken since 9c533be.

init.lua
@@ -171,16 +171,16 @@ function _M.createPreviewText(client)
 end
 
 -- Preview is created here.
-function _M.clientOpacity(altTabTable, altTabIndex)
+function _M.clientOpacity()
 	if not _M.settings.client_opacity then return end
 
 	local opacity = _M.settings.client_opacity_value
 	if opacity > 1 then opacity = 1 end
-	for i,data in pairs(altTabTable) do
+	for i,data in pairs(_M.altTabTable) do
 		data.client.opacity = opacity
 	end
 
-	if client.focus == altTabTable[altTabIndex].client then
+	if client.focus == _M.altTabTable[_M.altTabIndex].client then
 		-- Let's normalize the value up to 1.
 		local opacityFocusSelected = _M.settings.client_opacity_value_selected + _M.settings.client_opacity_value_in_focus
 		if opacityFocusSelected > 1 then opacityFocusSelected = 1 end
@@ -193,7 +193,7 @@ function _M.clientOpacity(altTabTable, altTabIndex)
 		if opacitySelected > 1 then opacitySelected = 1 end
 
 		client.focus.opacity = opacityFocus
-		altTabTable[altTabIndex].client.opacity = opacitySelected
+		_M.altTabTable[_M.altTabIndex].client.opacity = opacitySelected
 	end
 end
 